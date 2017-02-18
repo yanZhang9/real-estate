e511c139f49fc1a769163c52870bca9d3251f66a
yanZhang9 <yan.zhang@devinci.fr>
2017-02-18 01:27:09 +0100

    start TP3

--- app.js
+++ app.js
@@ -16,10 +16,25 @@ app.use( '/assets', express.static( 'assets' ) );
 //makes the server respond to the '/' route and serving the 'home.ejs' template in the 'views' directory
 app.get( '/', function ( req, res ) {
 
-    callLeboncoin();
+    callLeboncoin( function ( lbcData ) {
+        callMeilleursAgents( lbcData, res )
+    });
 
+    var url = req.query.urlLBC
+
+    //estimation();
     res.render( 'home', {
-        message: 'The Home Page!'
+        message: 'The Home Page!',
+        link: url
+    });
+});
+
+
+app.get( '/call', function ( req, res ) {
+
+    var url = req.query.urlLBC
+    res.render( 'home', {
+        message: url
     });
 });
 
@@ -40,15 +55,15 @@ request( 'http://www.google.com', function ( error, response, body ) {
 
 //fonction recupère le prix de l'annonce sur le site Leboncoin
 
-function callLeboncoin() {
-    var url = 'https://www.leboncoin.fr/ventes_immobilieres/1073837003.htm?ca=12_s'
+function callLeboncoin( receivedLBCData ) {
+    var urlLBC = 'https://www.leboncoin.fr/ventes_immobilieres/1073837003.htm?ca=12_s'
 
-    request( url, function ( error, response, html ) {
+    request( urlLBC, function ( error, response, html ) {
         if ( !error && response.statusCode == 200 ) {
 
-            const $ = cheerio.load( html )
+            var $ = cheerio.load( html )
 
-            const lbcDataArray = $( 'section.properties span.value' );
+            var lbcDataArray = $( 'section.properties span.value' );
 
             let lbcData = {
                 price: parseInt( $( lbcDataArray.get( 0 ) ).text().replace( /\s/g, '' ), 10 ),
@@ -57,6 +72,7 @@ function callLeboncoin() {
                 surface: parseInt( $( lbcDataArray.get( 4 ) ).text().replace( /\s/g, '' ), 10 )
             }
             console.log( lbcData )
+            receivedLBCData( lbcData )
 
 
             /*console.log($('h2.item_price span.value').text()); // show the text of the item
@@ -82,4 +98,52 @@ function callLeboncoin() {
         }
     })
 }
+
+function callMeilleursAgents() {
+    var urlMa = 'https://www.meilleursagents.com/prix-immobilier/nice-06000/'
+
+    request( urlMa, function ( error, response, html ) {
+        if ( !error && response.statusCode == 200 ) {
+
+            const $ = cheerio.load( html )
+
+            const maDataArray = $( 'div.prices-summary div.prices-summary__cell--median' );
+
+            let maData = {
+                price_m2_appart: parseInt( $( maDataArray.get( 0 ) ).text().replace( /\s/g, '' ), 10 ),
+                price_m2_house: parseInt( $( maDataArray.get( 1 ) ).text().replace( /\s/g, '' ), 10 ),
+                monthRent: parseFloat( $( maDataArray.get( 2 ) ).text().replace( /\s/g, '' ), 10 )
+
+            }
+            console.log( maData )
+            res.render( 'home', {
+
+            })
+        }
+
+        else {
+            console.log( error );
+        }
+    })
+}
+
+function estimation( surfaceLBC, prixLBC ) {
+    let prixM2_LBC = {
+        Estimation_M2_Price: parseInt( prixLBC / surfaceLBC )
+    }
+    console.log( prixM2_LBC )
+
+}
+
+/*
+function comparer(prixM2_LBC,typeLBC){
+    if(typeLBC=="appartement"){
+        var prix_MAgent= ;
+        if(prixM2_LBC)
+    }
+}
+
+
+*/
+
 
